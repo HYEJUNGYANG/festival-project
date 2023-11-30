62204f3f8478b4e3f7279e33f744be91227399b6
양혜정 <gpwjd248@gmail.com>
2023-11-30 10:16:18 +0900

    검색 기능 추가하려고 일부 수정 / 체험 디테일 페이지 리뷰 수정 및 삭제 부분 로그인 했을 때만 보여지도록 수정

--- src/main/java/com/festival/festival/controller/user/SearchController.java
+++ src/main/java/com/festival/festival/controller/user/SearchController.java
@@ -2,15 +2,18 @@ package com.festival.festival.controller.user;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/search")
+@RequestMapping("/")
 @Log4j2
 public class SearchController {
-    @GetMapping("")
-    public String search() {
+    @GetMapping("/search")
+    public String search(@RequestParam(value = "q", required = false)String q, Model model) {
         return "search";
     }
 }
