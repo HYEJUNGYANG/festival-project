bcbbd2cdd31a1bc59296bf4fc996796f9b0a8b8d
양혜정 <gpwjd248@gmail.com>
2023-12-05 17:23:04 +0900

    축제, 체험 상세 페이지 태그 부분 값 불러와서 반영되도록 수정

--- src/main/java/com/festival/festival/controller/user/ExperienceController.java
+++ src/main/java/com/festival/festival/controller/user/ExperienceController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -41,6 +42,23 @@ public class ExperienceController {
 
         ExpDTO expDTO = expService.read(idx);
         List<Review> reviewDto = reviewService.getList(idx);
+
+        //@@1@@로 되어있는 태그를 #1,#2로 변환
+        String savedTags = expDTO.getTag(); // 데이터베이스에서 읽어온 해시태그
+
+        String[] tags = savedTags.split("@@"); // "@@"로 해시태그 분리
+
+        List<String> taglist = new ArrayList<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (String tag : tags) {
+            if (!tag.isEmpty()) {
+                taglist.add("#"+tag); // 각 해시태그에서 "@@"를 제거하고 "#"을 추가하여 StringBuilder에 추가
+            }
+        }
+        String tag = sb.toString(); // 변환된 형식인 "#12,#34,#56" 형태의 문자열
+
+        model.addAttribute("taglist", taglist);
         model.addAttribute("dto", expDTO);
         model.addAttribute("review", reviewDto);
 
--- src/main/java/com/festival/festival/controller/user/FestivalController.java
+++ src/main/java/com/festival/festival/controller/user/FestivalController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -36,6 +37,23 @@ public class FestivalController {
     public String detail(@RequestParam(value = "idx", defaultValue = "1") Long idx, Model model) {
         if (idx != null) {
             FestivalDTO festivalDTO = festivalService.read(idx);
+
+            //@@1@@로 되어있는 태그를 #1,#2로 변환
+            String savedTags = festivalDTO.getTag(); // 데이터베이스에서 읽어온 해시태그
+
+            String[] tags = savedTags.split("@@"); // "@@"로 해시태그 분리
+
+            List<String> taglist = new ArrayList<>();
+
+            StringBuilder sb = new StringBuilder();
+            for (String tag : tags) {
+                if (!tag.isEmpty()) {
+                    taglist.add("#"+tag); // 각 해시태그에서 "@@"를 제거하고 "#"을 추가하여 StringBuilder에 추가
+                }
+            }
+            String tag = sb.toString(); // 변환된 형식인 "#12,#34,#56" 형태의 문자열
+
+            model.addAttribute("taglist", taglist);
             model.addAttribute("dto", festivalDTO);
         }
         return "/festival/festival-detail";
